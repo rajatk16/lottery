6ad4e0ce6caf07a88d59bce47bc2b8cfab2ba32a
Rajat Sudagade <rajat.k16@gmail.com>
2019-03-13 20:39:44 -0500

    first commit

---
+++ src/lottery.js
@@ -0,0 +1,79 @@
+import web3 from './web3';
+
+const address = '0xbCe909e60A1A672d26c7025C7a122DA4cc03e313';
+
+const abi = [
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"manager",
+    "outputs":[
+      {
+        "name":"",
+        "type":"address"
+      }
+    ],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "constant":false,
+    "inputs":[],
+    "name":"pickWinner",
+    "outputs":[],
+    "payable":false,
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"getPlayers",
+    "outputs":[
+      {
+        "name":"",
+        "type":"address[]"
+      }
+    ],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "constant":false,
+    "inputs":[],
+    "name":"enter",
+    "outputs":[],
+    "payable":true,
+    "stateMutability":"payable",
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[
+      {
+        "name":"",
+        "type":"uint256"
+      }
+    ],
+    "name":"players",
+    "outputs":[
+      {
+        "name":"",
+        "type":"address"
+      }
+    ],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "inputs":[],
+    "payable":false,
+    "stateMutability":"nonpayable",
+    "type":"constructor"
+  }
+]
+
+export default new web3.eth.Contract(abi, address);
